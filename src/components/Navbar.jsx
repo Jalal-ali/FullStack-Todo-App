f9b718021fab6509efc35bc8c8ca3309ab844e4f
Jallluu <alikhanumar795@gmail.com>
2024-09-24 04:21:46 +0500

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
 
   const [outBtn , setOutbtn] = useState(null)
   const [sign , setSign] = useState(null)
+  const [email , setEmail] = useState(null)
   const [hambrgr , setHambrgr] = useState(true)
   const navigate = useNavigate()
 
@@ -15,7 +16,9 @@ const Navbar = () => {
         console.log("logged in h");
         setSign(false)
         setOutbtn(true);
-        const uid = user.uid;
+          setEmail(user.email);
+        // console.log(email);
+        
       } else {
         setOutbtn(false);
         setSign(true)
@@ -30,9 +33,10 @@ const Navbar = () => {
     signOut(auth).then(() => {
       navigate("/login")
       console.log("logout hogya");
+      setEmail(null)
       
     }).catch((error) => {
-      alert("err");
+      alert("err" , error);
     });
   }
   const showBrgr = () => {
@@ -42,9 +46,9 @@ const Navbar = () => {
   return (
     <>
     <nav className="shadow-lg shadow-blue-300/50 bg-black dark:bg-gray-950">
-    <div className="max-w-screen-xl flex flex-wrap text-center justify-end mx-auto p-4">
-
-        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+    <div className="max-w-screen-xl flex flex-wrap text-center justify-between mx-auto p-4">
+<p className='text-white xl:text-xl cursor-pointer border-b border-cyan-800 first-letter:uppercase text-md font-light my-1 text-center'>{email}</p>
+        <div className="space-x-3 md:space-x-0 rtl:space-x-reverse">
             
           <button onClick={showBrgr} data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
@@ -55,7 +59,7 @@ const Navbar = () => {
         </div>
        {hambrgr ?  <div className="justify-center flex w-full md:flex md:w-auto md:order-1" itemID="navbar-cta">
           {sign ? <div>
-          <button type="button" ><Link className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" to="login">SignIn</Link></button>
+          <button type="button" ><Link className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-1" to="login">SignIn</Link></button>
               <button type='button'><Link className="relative p-0.5 inline-flex me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"  aria-current="page" to="register"><span className="relative p-2 px-3.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
 Sign-Up
 </span></Link></button>
@@ -64,7 +68,7 @@ Sign-Up
         
               
             
-{outBtn ? <button onClick={logout} type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Logout</button>
+{outBtn ? <button onClick={logout} type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-1">Logout</button>
 : null}
 
         </div> : null}

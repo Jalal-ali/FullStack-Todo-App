afd8e073793f8a5421b00b96a019fc5013a8699e
Jallluu <alikhanumar795@gmail.com>
2024-09-24 21:09:33 +0500

    database added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
     <>
     <nav className="shadow-lg shadow-blue-300/50 bg-black dark:bg-gray-950">
     <div className="max-w-screen-xl flex flex-wrap text-center justify-between mx-auto p-4">
-<p className='text-white xl:text-xl cursor-pointer border-b border-cyan-800 first-letter:uppercase text-md font-light my-1 text-center'>{email}</p>
+<p className='my-3 text-white xl:text-xl cursor-pointer first-letter:uppercase text-md font-light text-center'>{email}</p>
         <div className="space-x-3 md:space-x-0 rtl:space-x-reverse">
             
           <button onClick={showBrgr} data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
@@ -58,6 +58,8 @@ const Navbar = () => {
           </button>
         </div>
        {hambrgr ?  <div className="justify-center flex w-full md:flex md:w-auto md:order-1" itemID="navbar-cta">
+          
+          <button type="button" ><Link className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-3 mb-2" to="app">Home</Link></button>
           {sign ? <div>
           <button type="button" ><Link className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-1" to="login">SignIn</Link></button>
               <button type='button'><Link className="relative p-0.5 inline-flex me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"  aria-current="page" to="register"><span className="relative p-2 px-3.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
@@ -68,7 +70,7 @@ Sign-Up
         
               
             
-{outBtn ? <button onClick={logout} type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-1">Logout</button>
+{outBtn ? <button onClick={logout} type="button" className="my-2 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Logout</button>
 : null}
 
         </div> : null}
